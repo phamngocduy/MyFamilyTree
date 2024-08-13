945af300c8cca9ccabefec6d6aba3e950d537adc
phamngocduy <phmngcduy@gmail.com>
2024-08-13 22:43:11 +1000

    Add firebase to Index

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyD4WlWqGQ2vpr5avhOi5RigRvCBfetbisg',
+    authDomain: 'learn-firebase-6b3b4.firebaseapp.com',
+    projectId: 'learn-firebase-6b3b4',
+    storageBucket: 'learn-firebase-6b3b4.appspot.com',
+    messagingSenderId: '176093873193',
+    appId: '1:176093873193:web:2377cc57c19559d62e4838',
+    measurementId: 'G-39QLEL08ER'
+};
+const FIREBASE_VERSION = '10.12.5';
+const GLIENT_ID = '176093873193-3qt2s61p5n8bl4mpoa79k7ml0bfc08be.apps.googleusercontent.com';
