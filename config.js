29517603d0e38e772f0e10a48742e9cb7b0a87b0
phamngocduy <phmngcduy@gmail.com>
2024-08-15 22:06:54 +1000

    Fix bug local firebase-app.js

--- config.js
+++ config.js
@@ -5,7 +5,8 @@ const firebaseConfig = {
     storageBucket: 'learn-firebase-6b3b4.appspot.com',
     messagingSenderId: '176093873193',
     appId: '1:176093873193:web:2377cc57c19559d62e4838',
-    measurementId: 'G-39QLEL08ER'
+    measurementId: 'G-39QLEL08ER',
+    CLIENT_ID: '176093873193-3qt2s61p5n8bl4mpoa79k7ml0bfc08be.apps.googleusercontent.com'
 };
-const FIREBASE_VERSION = '10.12.5';
-const GLIENT_ID = '176093873193-3qt2s61p5n8bl4mpoa79k7ml0bfc08be.apps.googleusercontent.com';
+
+const FIREBASE = 'https://www.gstatic.com/firebasejs/10.12.5';
