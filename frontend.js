f1f76b89cdcff8db04e318529e8d88823e88a176
phamngocduy <phmngcduy@gmail.com>
2024-09-19 22:25:07 +1000

    Add creator and updator components

--- frontend.js
+++ frontend.js
@@ -1,18 +1,30 @@
 const {createApp} = await import('./packages/vue/dist/vue.esm-browser.prod.js');
 
+const Creator = {
+    props: {primary: Boolean},
+    template: document.getElementById('creator').innerHTML
+}
+
+const Updator = {
+    props: {primary: Boolean},
+    template: document.getElementById('updator').innerHTML
+}
+
 const Member = {
     name: 'Member',
+    components: {
+        Creator, Updator
+    },
     props: {
         self: Object,
         editor: Boolean
     },
+    created() {this.window = window},
     template: document.getElementById('member').innerHTML
 };
 
 export default createApp({
-    components: {
-        Member
-    },
+    components: {Member},
     data() {
         return {
             family: {},
